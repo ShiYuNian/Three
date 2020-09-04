e940424e161998b603ae75b44c5366defc24b540
白衣 <3363533398@qq.com>
2020-09-04 16:43:18 +0800

    登录页面

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {}
+}
+</script>
+
+<style lang="less"></style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// 导入通用的样式
+import './styles/base.less'
+import './styles/iconfont.css'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 导入amfe-flexible库
+import 'amfe-flexible'
+import HmHeader from './components/HmHeader.vue'
+import HmLogo from './components/HmLogo.vue'
+import axios from 'axios'
+
+// 把axios挂载到vue的原型
+Vue.prototype.$axios = axios
+// 给axios配置默认的baseURL ,基准地址
+axios.defaults.baseURL = 'http://localhost:3000'
+
+// 全局注册组件
+Vue.component('hm-header', HmHeader)
+Vue.component('hm-logo', HmLogo)
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
